672691beabf33fd32b9a60d8cad6883497238160
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2020-03-03 11:14:36 -0500

    Text transcription algo kinda works. Bugs at start/end

--- public/dnaEncode.js
+++ public/dnaEncode.js
@@ -6,7 +6,68 @@ class Base extends React.Component {
     return (<span className={"badge badge-pill badge-" + this.props.color}><h1>{this.props.letter}</h1></span>);
   }
 }
+class RealtimeTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentValue: ''
+    };
+  }
+  render() {
+    let bases = [];
+    if(this.state.binaryValue) {
+      for(let i = 0; i <= this.state.binaryValue.length; i += 2) {
+        bases.push(convertToDNA(this.state.binaryValue.substring(i, i + 2)));
+      }
+    }
+    return (
+      <div className="w-100">
+        <p className="lead">Enter regular text: </p>
+        <input
+          type="text" className="form-control" placeholder={this.props.prompt}
+          onChange={e => {
+            this.setState({ currentValue: e.target.value });
+            this.setState({ binaryValue : convertToBinary(this.state.currentValue)});
+          }}
+        />
+        <p>{this.state.binaryValue}</p>
+        <span>{bases}</span>
+        </div>
+    );
+  }
+}
+const BYTE_SIZE = 8;
+function convertToBinary(inputText) {
+  let binaryString = "";
+  for(let i = 0; i < inputText.length; i++) {
+    let convertedString = inputText.charCodeAt(i).toString(2);
+    for(let j = convertedString.length; j < BYTE_SIZE; j++) {
+      convertedString = "0" + convertedString; //zero left pad until reaching BYTE_SIZE
+    }
+    binaryString += convertedString;
+  }
+  return binaryString;
+}
+function convertToDNA(twoBitString) {
+  let result = <p></p>;
+  switch(twoBitString) {
+    case "00":
+    result = <Base color="primary" letter="A"/>;
+    break;
+    case "01":
+    result = <Base color="success" letter="G"/>;
+    break;
+    case "10":
+    result = <Base color="warning" letter="C"/>
+    break;
+    case "11":
+    result = <Base color="danger" letter="T"/>
+    break;
+  }
+  return result;
+}
+const textInput = <RealtimeTextInput prompt="Hello, DNA!"/>
 const bases = (<React.Fragment>
-            <Base color="primary" letter="A"/><Base color="success" letter="G"/><Base color="warning" letter="C"/><Base color="danger" letter="T"/>
+
            </React.Fragment>);
-ReactDOM.render(bases, document.getElementById('root'));
+ReactDOM.render(textInput, document.getElementById('root'));
